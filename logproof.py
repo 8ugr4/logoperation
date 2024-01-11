001abbee8b61899d51aa033766535d0cfe2d3bf7
8ugr4 <117930037+8ugr4@users.noreply.github.com>
2024-01-11 08:46:00 +0100

    Update logproof.py

--- logproof.py
+++ logproof.py
@@ -4,18 +4,16 @@
 # will always be >= -1
 from math import log2
 
-
-# print (math.log(-14))
 def operation(x):
     y= ( (x * log2(x)) + ((1-x) * log2(1-x)) )
     return y
+    
 def func():
     b=[]
     b += [i/10 for i in range(1,10)]
-    [print(i) for i in b]
+    [print(i) for i in b]  #prints out 0.1,0.2 ... 0.9
     c=[]
     c += ["{:.2f}".format(operation(elem)) for elem in b]
     return c
-    #a = [ elem*0.1 for elem in range(10) ]
-def logcheck(a):
-    return log2(a)
+    #c is
+    #['-0.47', '-0.72', '-0.88', '-0.97', '-1.00', '-0.97', '-0.88', '-0.72', '-0.47']
